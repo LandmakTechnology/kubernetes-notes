12da2617ef5d218fe0591e656ffbfd9fe2e3cfb5
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2021-08-01 11:55:37 -0400

    Update variables.tf

--- eks-setup-with-terraform/variables.tf
+++ eks-setup-with-terraform/variables.tf
@@ -7,7 +7,7 @@ variable "cluster-name" {
   type    = string
 }
 variable "key_pair_name" {
-  default = "ekskey"
+  default = "key22"
 }
 variable "eks_node_instance_type" {
   default = "t2.medium"
