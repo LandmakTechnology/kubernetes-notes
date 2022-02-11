d223b97743db8a5be69e19f424a0122630404b17
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2022-02-11 10:35:37 -0500

    Create eks-install_linux.sh

---
+++ eks-setup-with-terraform/eks-install_linux.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+# Install eksctl in ubuntu or any Linux
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
