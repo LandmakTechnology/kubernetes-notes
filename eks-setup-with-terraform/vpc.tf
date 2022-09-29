851057630115f452297bef65de5d317273d746b3
Steven Adewole <106304974+Eutechsystems@users.noreply.github.com>
2022-09-29 23:16:23 +0100

    Update vpc.tf

--- eks-setup-with-terraform/vpc.tf
+++ eks-setup-with-terraform/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "demo" {
   cidr_block = "10.0.0.0/16"
 
   tags = map(
-    "Name", "eks-landmark-node",
+    "Name", "eks-kongit-node",
     "kubernetes.io/cluster/${var.cluster-name}", "shared",
   )
 }
